1d541c0ffaa3f7b20fd84928af38d8afe27f4623
Shacent <hauzandini5@gmail.com>
2023-12-07 09:38:37 +0700

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,37 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Charles',
+    lastName: 'Dometian',
+    email: 'Domc@example.com',
+    salary: '95000',
+    date: '2019-04-11',
+  },
+  {
+    id: 2,
+    firstName: 'Hauzan',
+    lastName: 'Dini',
+    email: 'Hauzandini@example.com',
+    salary: '80000',
+    date: '2019-04-17',
+  },
+  {
+    id: 3,
+    firstName: 'Zalfa',
+    lastName: 'Pauninda',
+    email: 'ZalfaPaf@example.com',
+    salary: '79000',
+    date: '2019-05-01',
+  },
+  {
+    id: 4,
+    firstName: 'Richard',
+    lastName: 'Patriot',
+    email: 'RicPat@example.com',
+    salary: '56000',
+    date: '2019-05-03',
+  },
+  
+];
+
+export { employeesData };
